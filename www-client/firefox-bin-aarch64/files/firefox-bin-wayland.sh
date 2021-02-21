875f97021e17ddd2a1a17505ddc5b8b0a3df0b28
bing <bingquick@hotmail.com>
2021-02-22 07:47:20 +1100

    rm firefox-bin-aarch64

--- www-client/firefox-bin-aarch64/files/firefox-bin-wayland.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#
-# Run Mozilla Firefox (bin) on Wayland
-#
-export MOZ_ENABLE_WAYLAND=1
-exec /usr/bin/firefox-bin "$@"
--- www-client/firefox-bin-aarch64/files/firefox-bin-x11.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#
-# Run Mozilla Firefox (bin) on X11
-#
-export MOZ_DISABLE_WAYLAND=1
-exec /usr/bin/firefox-bin "$@"
